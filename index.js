db13bd97805c8b944e02c2360ffbe30407bcbe09
Chintav Shah <chintav96@hotmail.com>
2020-03-02 15:09:24 -0700

    Added changing nicknames

--- index.js
+++ index.js
@@ -7,24 +7,44 @@ var user = "User";
 var id = 1;
 
 app.get('/', function(req, res) {
+    console.log("Sending file..");
     res.sendFile(__dirname + '/index.html');
+    console.log("Sent file!");
+
+
 });
 
 io.on("connection", function(socket) {
     var nickname = user + id;
     console.log("A user has connected: " + nickname);
     id+=1;
-    online.push(nickname);    
-    io.emit("users", online);
+    online.push(nickname);
+    io.emit("users", online);    
+
 
     socket.on("disconnect", function() {
-        console.log("A user has disconnected"); 
+        console.log("A user has disconnected");
+        var index = online.indexOf(nickname);
+        var removed = online.splice(index, 1);
+        io.emit("users", online);
     })
     socket.on("chat message", function(msg) {
         console.log("message: " + msg);
         var msg_detail = new Object();
-        msg_detail.date = new Date(Date.now());
-        console.log(msg_detail.date);
+        var now = new Date(Date.now())
+        msg_detail.date = now.toLocaleString();
+        if(msg.startsWith('/nick')) {
+            var new_nickname = msg.split(" ")[1];
+            if (!online.includes(new_nickname)) {
+                var index = online.indexOf(nickname);
+                nickname = new_nickname;
+                online[index] = nickname;   
+                io.emit("users", online);
+            }
+            else {
+                socket.emit("nick_error");
+            }
+        }
         msg_detail.nick = nickname;
         msg_detail.message = msg;
         console.log(msg_detail);
@@ -35,5 +55,5 @@ io.on("connection", function(socket) {
 }); 
     
 http.listen(3000, function() {
-    console.log("listening on *:300");
+    console.log("listening on *:3000");
 });
